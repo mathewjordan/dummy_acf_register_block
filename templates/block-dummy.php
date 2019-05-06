e152d1ff18b379708fa479c38f8eb1cad04edd03
Mat Jordan <mat@utk.edu>
2019-05-06 08:53:34 -0400

    Update dummy template structure.

--- templates/block-dummy.php
+++ templates/block-dummy.php
@@ -7,10 +7,33 @@
     <div class="dummy-header">
         <h2><?php the_field('dummy_title'); ?></h2>
     </div>
-    <p class="dummy-body"><?php the_field('dummy_body'); ?></p>
     <div class="dummy-meta">
         <span><?php the_field('dummy_meta_1'); ?></span>
         <span><?php the_field('dummy_meta_2'); ?></span>
         <span><?php the_field('dummy_meta_3'); ?></span>
     </div>
 </div>
+<div class="dummy-block">
+    <div class="dummy-header">
+        <h2><?php the_field('dummy_title'); ?></h2>
+    </div>
+    <ul class="dummy-meta">
+        <li><?php the_field('dummy_meta_1'); ?></li>
+        <li><?php the_field('dummy_meta_2'); ?></li>
+        <li><?php the_field('dummy_meta_3'); ?></li>
+    </ul>
+</div>
+<?php
+
+echo '<div class="dummy-block">';
+    echo '<div class="dummy-header">';
+        echo '<h2>' . get_field('dummy_title') . '</h2>';
+    echo '</div>';
+    echo '<div class="dummy-meta">';
+        echo '<span>' . get_field('dummy_meta_1') . '</span>';
+        echo '<span>' . get_field('dummy_meta_2') . '</span>';
+        echo '<span>' . get_field('dummy_meta_3') . '</span>';
+    echo '</div>';
+echo '</div>';
+
+?>
